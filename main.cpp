661ef4b016ee5bfaf6bbb1a146d5c484c85fd763
danielzelfo <daniel.zelfo@gmail.com>
2019-04-20 13:58:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-/*  Program: Store and Employee Bonus Program v1.0
+/*  Program: Store and Employee Bonus Program v1.1 (EXTRA CREDIT)
 Author: Daniel Zelfo
 Class: CSCI 110
 Date: 10/3/2018
 Description: Displays store and employee bonus using input of monthly sales and sales increase.
+	version 1.1: added levels to store and emploee bonus (EXTRA CREDIT)
 
 I certify that the code below is my own work.
 */
@@ -21,7 +22,7 @@ int main()
 {
 	char keepGoing = 'y';
 	double monthlySales, storeAmount, salesIncrease, empAmount;
-	cout << "Store and Employee Bonus Program. v1.0\nAuthor: Daniel Zelfo\n";
+	cout << "Store and Employee Bonus Program. v1.1\nAuthor: Daniel Zelfo\n";
 
 	while (keepGoing == 'y')
 	{
@@ -45,10 +46,16 @@ void getSales(double& monthlySales)
 
 void storeBonus(double monthlySales, double& storeAmount)
 {
-	if (monthlySales >= 100000)
+	if (monthlySales >= 110000)
+		storeAmount = 6000;
+	else if (monthlySales >= 100000)
 		storeAmount = 5000;
+	else if (monthlySales >= 90000)
+		storeAmount = 4000;
+	else if (monthlySales >= 80000)
+		storeAmount = 3000;
 	else
-		storeAmount = 500;
+		storeAmount = 0;
 }
 
 void getIncrease(double& salesIncrease)
@@ -59,8 +66,12 @@ void getIncrease(double& salesIncrease)
 
 void empBonus(double salesIncrease, double& empAmount)
 {
-	if (salesIncrease >= 0.04)
+	if (salesIncrease >= 0.05)
+		empAmount = 75;
+	else if (salesIncrease >= 0.04)
 		empAmount = 50;
+	else if (salesIncrease >= 0.03)
+		empAmount = 40;
 	else
 		empAmount = 0;
 }
@@ -69,4 +80,6 @@ void printBonus(double storeAmount, double empAmount)
 {
 	cout << "The store bonus is $" << storeAmount << endl;
 	cout << "The employee bonus is $" << empAmount << endl;
+	if (storeAmount == 6000 && empAmount == 75)
+		cout << "Congrats! You have reached the highest bonus amounts possible!\n";
 }
